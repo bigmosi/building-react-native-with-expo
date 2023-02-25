b1b6d3eba4cb52a42042208a7c8d14fea7231715
bigmosi <kinyeramo@gmail.com>
2023-02-26 01:35:38 +0300

    import the screens

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import HomeScreen from './src/screens/HomeScreen';
+import DetailsScreen from './src/screens/DetailsScreen';
+import ImageScreen from './src/screens/ImageScreen';
+
 export default function App() {
   return (
     <View style={styles.container}>
