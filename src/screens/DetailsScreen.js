bcc061f41762c0564aa6cb8579c3e13963eeede3
bigmosi <kinyeramo@gmail.com>
2023-02-26 18:07:37 +0300

    added params

--- src/screens/DetailsScreen.js
+++ src/screens/DetailsScreen.js
@@ -3,14 +3,18 @@ import { Button, View, Text, StyleSheet } from 'react-native';
 
 const DetailsScreen = ({navigation, route}) => {
   console.log(route);
+
+  const { screenNumber } = route.params;
+
     return (
       <View style={styles.mainView}>
         <Text>Details Screen</Text>
+        <Text style={{fontSize: 100}} >{ screenNumber }</Text>
           <Button
             title="Go to Image"
             onPress={() => {navigation.navigate("BigImageView")}}/>
           <Button title="More Details" onPress={() => {navigation.push("Details_to_Details")}} />
-          <Button title="Go to Details" onPress={() => {navigation.goBack("Details_to_Details")}} />
+          <Button title="Go Back Home" onPress={() => {navigation.popToTop()}} />
 
       </View>
     )
